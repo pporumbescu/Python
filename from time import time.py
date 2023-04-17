04701fffab8f01029d483e58250a6eb12cf7e2a6
pporumbescu <60489736+pporumbescu@users.noreply.github.com>
2023-04-17 15:12:17 +0100

    1st

--- from time import time.py
+++ from time import time.py
@@ -1,23 +1,30 @@
-from time import time
+import time
+
+
 def performance(fn):
     def wrapper(*args, **kawrgs):
-        t1 = time()
+        t1 = time.time()
         result = fn(*args, **kawrgs)
-        t2 = time()
-        print(f'took {t2-t1} s')
+        t2 = time.time()
+        print(f'took {t2 - t1} s')
         return result
+
     return wrapper
+
 
 @performance
 def long_time():
     print('1')
     for i in range(10000000):
-        i*5
+        i * 5
+
+
 @performance
 def long_time2():
     print('2')
     for i in list(range(10000000)):
-        i*5
+        i * 5
+
 
 long_time()
 long_time2()
